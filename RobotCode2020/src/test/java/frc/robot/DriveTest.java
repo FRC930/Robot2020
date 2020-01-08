85ddafbd7a855250d057519677b11666ec769261
Travis Groshek <travgros@gmail.com>
2020-01-07 20:35:51 -0600

    Fixed and documented DriveTest class

--- RobotCode2020/src/test/java/frc/robot/DriveTest.java
+++ RobotCode2020/src/test/java/frc/robot/DriveTest.java
@@ -13,6 +13,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.withSettings;
 
+import java.lang.management.ManagementFactory;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -24,11 +26,14 @@ import org.mockito.ArgumentMatcher;
  * <h3>DriveTest</h3>
  * 
  * DriveTest tests interfaces in the Drive class <br>
- * Just changed the method for testing for correct values Now we use the verify
- * method to check for correct values
+ * DriveTest tests mocked Spark Maxes with drivetrain
+ * code. It makes sure that the different motors for
+ * each drivetrain wheel will run properly.
+ * 
  */
 public class DriveTest {
-    
+
+    //-------- DECLARATIONS --------\\
     private Drive drive;
     private CANSparkMax right1;
     private CANSparkMax right2;
@@ -42,18 +47,12 @@ public class DriveTest {
      */
     public void setHardware() {
         // Mocking Spark Max motor controllers
-        this.right1 = mock(CANSparkMax.class,
-                withSettings().useConstructor(4, MotorType.kBrushless));
-        this.right2 = mock(CANSparkMax.class,
-                withSettings().useConstructor(5, MotorType.kBrushless));
-        this.right3 = mock(CANSparkMax.class,
-                withSettings().useConstructor(6, MotorType.kBrushless));
-        this.left1 = mock(CANSparkMax.class,
-                withSettings().useConstructor(1, MotorType.kBrushless));
-        this.left2 = mock(CANSparkMax.class,
-                withSettings().useConstructor(2, MotorType.kBrushless));
-        this.left3 = mock(CANSparkMax.class,
-                withSettings().useConstructor(3, MotorType.kBrushless));
+        right1 = mock(CANSparkMax.class);
+        right2 = mock(CANSparkMax.class);
+        right3 = mock(CANSparkMax.class);
+        left1 = mock(CANSparkMax.class);
+        left2 = mock(CANSparkMax.class);
+        left3 = mock(CANSparkMax.class);
     }
 
     /**
@@ -62,193 +61,195 @@ public class DriveTest {
      */
     @Before
     public void setup() {
-        this.drive = Drive.getInstance();
-        this.setHardware();
-        this.drive.setMotorControllers(left1, left2, left3, right1, right2, right3);
+        drive = Drive.getInstance();
+        setHardware();
+        drive.setMotorControllers(left1, left2, left3, right1, right2, right3);
     }
 
     //-------- TESTS --------\\
 
     /**
-     * This test checks that the robot holds to the joystick deadband
+     * This test checks that the robot holds to the right joystick deadband
      */
     @Test
     public void rightJoystick() {
         // No joystick movement
-        this.drive.run(0, 0);
+        drive.run(0, 0);
 
         // Very small joystick movement just below the deadband, still shouldn't be
         // enough to trigger movement
-        this.drive.run(0, 0.000123);
-        this.drive.run(0, -0.000123);
+        drive.run(0, 0.000123);
+        drive.run(0, -0.000123);
 
         // Verify that there are three (3) zero values
-        verify(this.left1, times(3)).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1, times(3)).set(doubleThat(new CustomDouble(0)));
+        verify(left1, times(3)).set(doubleThat(new CustomDouble(0)));
+        verify(right1, times(3)).set(doubleThat(new CustomDouble(0)));
 
         // Run at the deadband
         // The robot should set the motor controllers to a non-zero value
-        this.drive.run(0, 0.000124);
-        this.drive.run(0, -0.000124);
+        drive.run(0, 0.000124);
+        drive.run(0, -0.000124);
 
         // Verify that there are no (0) nonzero values
-        verify(this.left1, times(0)).set(doubleThat(new NonZeroDouble()));
-        verify(this.right1, times(0)).set(doubleThat(new NonZeroDouble()));
+        verify(left1, times(0)).set(doubleThat(new NonZeroDouble()));
+        verify(right1, times(0)).set(doubleThat(new NonZeroDouble()));
 
         // Run at a value above the deadband
         // Returns nonzero
-        this.drive.run(0, 0.001);
-        this.drive.run(0, -0.001);
+        drive.run(0, 0.001);
+        drive.run(0, -0.001);
 
         // Verify that there are five (5) zero values 
-        verify(this.left1, times(5)).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1, times(5)).set(doubleThat(new CustomDouble(0)));
+        verify(left1, times(5)).set(doubleThat(new CustomDouble(0)));
+        verify(right1, times(5)).set(doubleThat(new CustomDouble(0)));
 
         // Verify that there are two (2) nonzero values
-        verify(this.left1, times(2)).set(doubleThat(new NonZeroDouble()));
-        verify(this.right1, times(2)).set(doubleThat(new NonZeroDouble()));
+        verify(left1, times(2)).set(doubleThat(new NonZeroDouble()));
+        verify(right1, times(2)).set(doubleThat(new NonZeroDouble()));
     }
 
     /**
-     * This test checks that the robot holds to the joystick deadband
+     * This test checks that the robot holds to the left joystick deadband
      */
     @Test
     public void leftJoystick() {
         // No joystick movement
-        this.drive.run(0, 0);
+        drive.run(0, 0);
 
         // Very small joystick movement just below the deadband, still shouldn't be
         // enough to trigger movement
-        this.drive.run(0.000123, 0);
-        this.drive.run(-0.000123, 0);
+        drive.run(0.000123, 0);
+        drive.run(-0.000123, 0);
 
         // Verify that there are three (3) zero values
-        verify(this.left1, times(3)).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1, times(3)).set(doubleThat(new CustomDouble(0)));
+        verify(left1, times(3)).set(doubleThat(new CustomDouble(0)));
+        verify(right1, times(3)).set(doubleThat(new CustomDouble(0)));
 
         // Run at the deadband
         // The robot should set the motor controllers to a non-zero value
-        this.drive.run(0.000124, 0);
-        this.drive.run(-0.000124, 0);
+        drive.run(0.000124, 0);
+        drive.run(-0.000124, 0);
 
         // Verify that there are no (0) nonzero values
-        verify(this.left1, times(0)).set(doubleThat(new NonZeroDouble()));
-        verify(this.right1, times(0)).set(doubleThat(new NonZeroDouble()));
+        verify(left1, times(0)).set(doubleThat(new NonZeroDouble()));
+        verify(right1, times(0)).set(doubleThat(new NonZeroDouble()));
 
         // Verify that there are five (5) zero values
-        verify(this.left1, times(5)).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1, times(5)).set(doubleThat(new CustomDouble(0)));
+        verify(left1, times(5)).set(doubleThat(new CustomDouble(0)));
+        verify(right1, times(5)).set(doubleThat(new CustomDouble(0)));
 
         // Run at a value above the deadband
-        this.drive.run(0.001, 0);
-        this.drive.run(-0.001, 0);
+        drive.run(0.001, 0);
+        drive.run(-0.001, 0);
 
         // Verify that there are two (2) nonzero values     
-        verify(this.left1, times(2)).set(doubleThat(new NonZeroDouble()));
-        verify(this.right1, times(2)).set(doubleThat(new NonZeroDouble()));
+        verify(left1, times(2)).set(doubleThat(new NonZeroDouble()));
+        verify(right1, times(2)).set(doubleThat(new NonZeroDouble()));
     }
 
     /**
-     * Test both of the joysticks to make sure that the robot sets the values
-     * correctly
+     * This test checks that the robot holds to both joystick deadband 
      */
     @Test
     public void testBothJoysticks() {
         // Very small joystick movement just below the deadband, still shouldn't be
         // enough to trigger movement
-        this.drive.run(0.000123, 0.000123);
-        this.drive.run(-0.000123, -0.000123);
+        drive.run(0.000123, 0.000123);
+        drive.run(-0.000123, -0.000123);
 
         // Small value much below the deadband
-        this.drive.run(0.00001, 0.00001);
-        this.drive.run(-0.00001, -0.00001);
+        drive.run(0.00001, 0.00001);
+        drive.run(-0.00001, -0.00001);
 
         // Verify that there are four (4) zero values
-        verify(this.left1, times(4)).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1, times(4)).set(doubleThat(new CustomDouble(0)));
+        verify(left1, times(4)).set(doubleThat(new CustomDouble(0)));
+        verify(right1, times(4)).set(doubleThat(new CustomDouble(0)));
 
         // Run at the deadband
         // The robot should set the motor controllers to a non-zero value
-        this.drive.run(0.000124, 0.000124);
-        this.drive.run(-0.000124, 0.000124);
+        drive.run(0.000124, 0.000124);
+        drive.run(-0.000124, 0.000124);
 
         // Verify that there are four (4) zero values
-        verify(this.left1, times(0)).set(doubleThat(new NonZeroDouble()));
-        verify(this.right1, times(0)).set(doubleThat(new NonZeroDouble()));
+        verify(left1, times(0)).set(doubleThat(new NonZeroDouble()));
+        verify(right1, times(0)).set(doubleThat(new NonZeroDouble()));
 
         // Run at a value above the deadband
-        this.drive.run(0.001, 0.001);
-        this.drive.run(-0.001, -0.001);
+        drive.run(0.001, 0.001);
+        drive.run(-0.001, -0.001);
 
         // Verify that there are two (2) nonzero values
-        verify(this.left1, times(2)).set(doubleThat(new NonZeroDouble()));
-        verify(this.right1, times(2)).set(doubleThat(new NonZeroDouble()));
+        verify(left1, times(2)).set(doubleThat(new NonZeroDouble()));
+        verify(right1, times(2)).set(doubleThat(new NonZeroDouble()));
 
         // Verify that there are six (6) zero values
-        verify(this.left1, times(6)).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1, times(6)).set(doubleThat(new CustomDouble(0)));
+        verify(left1, times(6)).set(doubleThat(new CustomDouble(0)));
+        verify(right1, times(6)).set(doubleThat(new CustomDouble(0)));
     } //    end of test testBothJoysticks()
 
     /**
-     * Test to make sure that the robot moves when it is supposed to.
+     * Test to make sure that the robot moves when both joysticks
+     * take in values. *Each of the input values below are cubed*
      */
     @Test
     public void testMovement() {
         // Test with the left joystick at zero
-        // Right joystick at a negative value
-        this.drive.run(0, -0.5);
-        verify(this.left1).set(doubleThat(new CustomDouble(-0.125)));
-        verify(this.right1).set(doubleThat(new CustomDouble(0.125)));
+        // Right joystick in the left position
+        drive.run(0, -0.5);
+        verify(left1).set(doubleThat(new CustomDouble(-0.125)));
+        verify(right1).set(doubleThat(new CustomDouble(0.125)));
 
         // Right joystick at zero
-        this.drive.run(0, 0);
-        verify(this.left1).set(doubleThat(new CustomDouble(0)));
-        verify(this.right1).set(doubleThat(new CustomDouble(0)));
+        drive.run(0, 0);
+        verify(left1).set(doubleThat(new CustomDouble(0)));
+        verify(right1).set(doubleThat(new CustomDouble(0)));
 
-        // Right joystick at a positive value
-        this.drive.run(0, 0.5);
-        verify(this.left1).set(doubleThat(new CustomDouble(0.125)));
-        verify(this.right1).set(doubleThat(new CustomDouble(-0.125)));
+        // Right joystick in the right position
+        drive.run(0, 0.5);
+        verify(left1).set(doubleThat(new CustomDouble(0.125)));
+        verify(right1).set(doubleThat(new CustomDouble(-0.125)));
 
-        // Test with the left joystick positive
+        // Test with the left joystick up
         // Right joystick at a negative value
-        this.drive.run(0.5, -0.5);
-        verify(this.left1).set(doubleThat(new CustomDouble(-0.21625)));
-        verify(this.right1).set(doubleThat(new CustomDouble(0.03375)));
+        drive.run(0.5, -0.5);
+        verify(left1).set(doubleThat(new CustomDouble(-0.21625)));
+        verify(right1).set(doubleThat(new CustomDouble(0.03375)));
 
         // Right joystick at zero
-        this.drive.run(0.5, 0);
-        verify(this.left1).set(doubleThat(new CustomDouble(-0.09125)));
-        verify(this.right1).set(doubleThat(new CustomDouble(-0.09125)));
+        drive.run(0.5, 0);
+        verify(left1).set(doubleThat(new CustomDouble(-0.09125)));
+        verify(right1).set(doubleThat(new CustomDouble(-0.09125)));
 
-        // Right joystick at a positive value
-        this.drive.run(0.5, 0.5);
-        verify(this.left1).set(doubleThat(new CustomDouble(0.03375)));
-        verify(this.right1).set(doubleThat(new CustomDouble(-0.21625)));
+        // Right joystick in the right position
+        drive.run(0.5, 0.5);
+        verify(left1).set(doubleThat(new CustomDouble(0.03375)));
+        verify(right1).set(doubleThat(new CustomDouble(-0.21625)));
 
-        // Test with the left joystick negative
-        // Right joystick at a negative value
-        this.drive.run(-0.5, -0.5);
-        verify(this.left1).set(doubleThat(new CustomDouble(-0.03375)));
-        verify(this.right1).set(doubleThat(new CustomDouble(0.21625)));
+        // Test with the left joystick down
+        // Right joystick in the left position
+        drive.run(-0.5, -0.5);
+        verify(left1).set(doubleThat(new CustomDouble(-0.03375)));
+        verify(right1).set(doubleThat(new CustomDouble(0.21625)));
 
         // Right joystick at zero
-        this.drive.run(-0.5, 0);
-        verify(this.left1).set(doubleThat(new CustomDouble(0.09125)));
-        verify(this.right1).set(doubleThat(new CustomDouble(0.09125)));
+        drive.run(-0.5, 0);
+        verify(left1).set(doubleThat(new CustomDouble(0.09125)));
+        verify(right1).set(doubleThat(new CustomDouble(0.09125)));
 
-        // Right joystick at a positive value
-        this.drive.run(-0.5, 0.5);
-        verify(this.left1).set(doubleThat(new CustomDouble(0.21625)));
-        verify(this.right1).set(doubleThat(new CustomDouble(-0.03375)));
+        // Right joystick in the right position
+        drive.run(-0.5, 0.5);
+        verify(left1).set(doubleThat(new CustomDouble(0.21625)));
+        verify(right1).set(doubleThat(new CustomDouble(-0.03375)));
     } //    end of test testMovement()
 } //    end of DriveTest() class
 
 /**
  * <h3>NonZeroDouble</h3>
  * 
- * NonZeroDouble is an argument matcher that returns true if the number is not
- * zero
+ * NonZeroDouble is an method that returns true if the drive.run
+ * input is not zero. It will return a nonzero value if the value
+ * in the drive.run method is outside the deadband (-0.001, 0.001)
+ * 
  */
 class NonZeroDouble implements ArgumentMatcher<Double> {
     @Override
@@ -264,19 +265,21 @@ class NonZeroDouble implements ArgumentMatcher<Double> {
 /**
  * <h3>CustomDouble</h3>
  * 
- * CustomDouble is an argument matcher that allows the user to input a double
- * that they want to compare to in the constructor.
+ * CustomDouble is an method that returns true if the drive.run
+ * input is zero. It will return zero if the value in the drive.run
+ * method is within the deadband (-0.001, 0.001)
+ * 
  */
 class CustomDouble implements ArgumentMatcher<Double> {
     private double number;
 
     public CustomDouble(double d) {
-        this.number = d;
+        number = d;
     }
 
     @Override
     public boolean matches(Double argument) {
-        if (Math.abs(argument - this.number) < Math.ulp(1.0)) {
+        if (Math.abs(argument - number) < Math.ulp(1.0)) {
             return true;
         } else {
             return false;
