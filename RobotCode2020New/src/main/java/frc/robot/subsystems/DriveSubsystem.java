510abd10c2aa18693589ce1198d1862535810151
Kyle Sene <soupcateatternom@gmail.com>
2020-01-11 10:56:31 -0600

    Added Drive example subsystem

---
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    //-------- CONSTANTS --------\\
+
+    //-------- DECLARATIONS --------\\
+
+    //-------- CONSTRUCTOR --------\\
+
+    public DriveSubsystem() {
+
+    }
+
+    //-------- METHODS --------\\
+    
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+    }
+}
