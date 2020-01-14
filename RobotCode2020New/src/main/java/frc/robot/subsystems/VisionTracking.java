992443fbb671d7ce319821f1f843f87e41c5ea17
aesmeier7 <abigator2020@gmail.com>
2020-01-13 19:42:16 -0600

    Final setup

--- RobotCode2020New/src/main/java/frc/robot/subsystems/VisionTracking.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/VisionTracking.java
@@ -9,17 +9,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
- */
 public class VisionTracking extends SubsystemBase {
 
   //-------- CONSTANTS --------\\
 
-   //--Ports
+    //--Ports
 
   //-------- DECLARATIONS --------\\
 
