1293d6e2573675afd9b3ae4ee0a1c0835c86313a
aesmeier7 <abigator2020@gmail.com>
2020-01-14 17:53:15 -0600

    Starting limelight and moving robot

--- RobotCode2020New/src/main/java/frc/robot/subsystems/VisionTracking.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/VisionTracking.java
@@ -8,20 +8,52 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 
 public class VisionTracking extends SubsystemBase {
 
   //-------- CONSTANTS --------\\
+    private final double If_You_See_This_Code_No_Work = 0.12345;
 
     //--Ports
 
   //-------- DECLARATIONS --------\\
+  
+ // tv  Whether the limelight has any valid targets (0 or 1)
+ private double tv;
+ // tx  Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+ private double tx;
+ // ty  Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+ private double ty;
+ // ta  Target Area (0% of image to 100% of image)
+ private double ta;
+ // ts  Skew or rotation (-90 degrees to 0 degrees)
+ private double ts;
+ // tl  The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.
+ private double tl;
+ 
+  
 
   public VisionTracking() {
 
   }
   //-------- METHODS --------\\
- 
+ // Whether the limelight has any valid targets (0 or 1) 
+ NetworkTableEntry tv = double.getEntry("tv");
+ // Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees) 
+ NetworkTableEntry tx = double.getEntry("tx");
+ // Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees) 
+ NetworkTableEntry ty = double.getEntry("ty");
+ // Target Area (0% of image to 100% of image) 
+ NetworkTableEntry ta = double.getEntry("ta");
+ // Skew or rotation (-90 degrees to 0 degrees) 
+ NetworkTableEntry ts = double.getEntry("ts");
+ // The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency. 
+ NetworkTableEntry tl = double.getEntry("tl");
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
