8fe4e3cd85936723aec42d5258d50ef4cbf72444
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2020-01-21 18:41:24 -0600

    Finished comments in Turret subsystem
    Co-authored-by: wizardwarior101 <wizardwarior10@gmail.com>
    Co-authored-by: savojosh <jmps2022@gmail.com>

--- RobotCode2020New/src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -7,11 +7,20 @@ import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
 /**
- * TurretSubsystem
+ * <h2>TurretSubsystem
+ * 
+ * <p>TurretSubsystem is a PID subsystem that controls the rotation of the turret
  */
 public class TurretSubsystem extends PIDSubsystem {
-    CANSparkMax controller;
+    /**
+     * The motor controller that will control the turret rotation
+     */
+    private CANSparkMax controller;
 
+    /**
+     * <p>Use this constructor to initialize the PID controller to a default controller
+     * <p>This constructor also calls {@link #setHardware() setHardware}
+     */
     public TurretSubsystem() {
         // TODO: Figure out values for PID controller
         super(new PIDController(0, 0, 0));
@@ -19,25 +28,49 @@ public class TurretSubsystem extends PIDSubsystem {
         setSetpoint(0 /* Figure out the set-point */);
     }
 
+    /**
+     * Use this controller to initialize the PID controller to a specified controller
+     * 
+     * @param controller is a PIDController that will control the turret
+     */
     public TurretSubsystem(PIDController controller) {
         super(controller);
     }
 
+    /**
+     * Use this method to set the motor controller to a default
+     */
     public void setHardware() {
-        // TODO: Get device IDs
+        // TODO: Get device ID
         setHardware(new CANSparkMax(0 /* Figure out device ID */, MotorType.kBrushless));
     }
 
+    /**
+     * Use this method to set the motor controller to a passed motor controller
+     * 
+     * @param turretController is the motor controller
+     */
     public void setHardware(CANSparkMax turretController) {
         controller = turretController;
     }
 
+    /**
+     * This method sets the motor controller speed to the output of the PID controller 
+     * 
+     * @param output the output that the PID controller sends
+     * @param setPoint is the value that the PID controller is trying to accomplishd
+     */
     @Override
     protected void useOutput(double output, double setPoint) {
-        // Check this. Could need to be negative output
+        // TODO: Check this. Could need to be negative output
         controller.set(output);
     }
 
+    /**
+     * This method returns the measurement that the PID controller will use to try to achieve the setpoint
+     * 
+     * @return the measurement given from the LimeLight
+     */
     @Override
     protected double getMeasurement() {
         // TODO: Talk to Andrew's subteam to get the angle value from the LimeLight
