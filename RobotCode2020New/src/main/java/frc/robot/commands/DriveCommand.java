70500caaaab075033cbda84b7b808fb49cc2697c
Nathankitzman <nkitzman11@gmail.com>
2020-01-22 19:51:53 -0600

    added some auto code back in

--- RobotCode2020New/src/main/java/frc/robot/commands/DriveCommand.java
+++ RobotCode2020New/src/main/java/frc/robot/commands/DriveCommand.java
@@ -19,20 +19,15 @@ public class DriveCommand extends CommandBase {
   //-------- CONSTANTS --------\\
 
   //-------- DECLARATIONS --------\\
-
   private final DriveSubsystem driveSubsystem;
   private final Joystick m_driveStick;
   //-------- CONSTRUCTOR --------\\
-
   public DriveCommand(DriveSubsystem dSubsystem,Joystick driverStick) {
     driveSubsystem = dSubsystem;
     m_driveStick = driverStick;
     addRequirements(dSubsystem);  // Use addRequirements() here to declare subsystem dependencies.
   } 
-
   //-------- COMMANDBASE METHODS --------\\
-
-  
   @Override   // Called when the command is initially scheduled.
   public void initialize() {
   }
@@ -41,26 +36,18 @@ public class DriveCommand extends CommandBase {
   public void execute() {  
     run(m_driveStick.getRawAxis(Constants.AXIS_RIGHT_X), m_driveStick.getRawAxis(Constants.AXIS_LEFT_Y));
   }
-
-  
   @Override   // Called once the command ends or is interrupted.
   public void end(boolean interrupted) {
   }
   //We comment out isFinished so we always have control of the drive and it gives contorl up easier
-  
   @Override   // Returns true when the command should end.
   public boolean isFinished() {
     return false;
   }
-  
-  
-
   //-------- METHODS --------\\
   private void run(double stickX, double stickY) {
 
     //-----------------------------------------------------------------CUBING
-    
-
     // Cubing values to create smoother function
     stickX = -Math.pow(stickX, 3);
     stickY = Math.pow(stickY, 3);
@@ -75,53 +62,7 @@ public class DriveCommand extends CommandBase {
     if (Math.abs(stickY) < Constants.DRIVE_DEADBAND_JOYSTICK) {
       stickY = 0;
     }
-
     // Arcade drive
     driveSubsystem.runAt((stickY + stickX), -(stickY - stickX));
-    
-    //-----------------------------------------------------------------CUBING
-
-    /*
-    // Joystick deadband
-    if (Math.abs(stickX) < Constants.DRIVE_DEADBAND_JOYSTICK) {
-      //System.out.println("Dead band");
-      stickX = 0;
-    }
-
-    if (Math.abs(stickY) < Constants.DRIVE_DEADBAND_JOYSTICK) {
-     // System.out.println("Dead band");
-      stickY = 0;
-    }
-    
-    //stickX = -lerp(prevX, stickX, 1);
-    stickX = -Math.pow(stickX, 3);
-    stickX *= Constants.DRIVE_TURNING_MULTIPLIER;
-
-    stickY = lerp(prevY, stickY, 0.07);
-
-    prevX = stickX;
-    prevY = stickY;
-
-    driveSubsystem.runAt((stickY + stickX), -(stickY - stickX));
-
-    */
-    
-    //stickX *= Constants.DRIVE_TURNING_MULTIPLIER;
-    
-    /*
-    if (Math.abs(stickX) < Constants.DRIVE_DEADBAND_JOYSTICK) {
-      //System.out.println("Dead band");
-      stickX = 0;
-    }
-    if (Math.abs(stickY) < Constants.DRIVE_DEADBAND_JOYSTICK) {
-     // System.out.println("Dead band");
-      stickY = 0;
-    }
-    */
-     
-    
-
-
   } //End of method run()
-
 } //End of class DriveCommand
--- RobotCode2020New/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -20,7 +20,10 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 
 public class DriveSubsystem extends SubsystemBase {
@@ -103,20 +106,20 @@ public class DriveSubsystem extends SubsystemBase {
         right2.follow(right1); 
         //right3.follow(right1);
 
-        //left1.setOpenLoopRampRate(0.5);
-        //right1.setOpenLoopRampRate(0.5);
-
+        left1.configOpenloopRamp(0.5);
+        right1.configOpenloopRamp(0.5);
         //m_drive = new DifferentialDrive(right1, left1);
     }
 
     // Given Arcade value arguments and sends to motor controllers
     public void runAt(double leftSpeed, double rightSpeed) {
+        System.out.println(left1.getMotorOutputPercent());
         left1.set(TalonFXControlMode.PercentOutput,leftSpeed);
         right1.set(TalonFXControlMode.PercentOutput,rightSpeed);
     }
-    // Returns left speed
+    //Returns left speed
     // public double getLeftSpeed() {
-    //     return left1.get();
+    //     return left1.getMotorOutputPercent();
     // }
 
     // Returns right speed
@@ -130,29 +133,34 @@ public class DriveSubsystem extends SubsystemBase {
     // public DifferentialDriveWheelSpeeds getWheelSpeeds() {
     //   return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
     // }
-    // public double getHeading() {
-    //   m_gyro.getYawPitchRoll(values);
-    //   return Math.IEEEremainder(values[0], 360) * (false ? -1.0 : 1.0);
-    // }
-    // public void resetOdometry(Pose2d pose) {
-    //   resetEncoders();
-    //   m_odometry.resetPosition(pose, Rotation2d.fromDegrees(getHeading()));
-    // }
+    public double getRPM(double vel)
+  {
+    // The velocity from the falcon encoder doesn't give use RPM, but rather their own units. There are 2048 position units per 100ms.
+    // One unit = one position unit / 100ms; One revolution per 2048 units;
+    // So if we do, (1 Revolution/ 2048)*(1000ms/1s)*(60s/1min) that converts our revolutions to minutes.
+    // The conversion will result in 60000/204800 (600/2048 simplified (as seen in the return statement))
+    // So our final equation will be (getIntergratedSensorVelocity() * (600.0 / 2048.0)) resulting in a pretty accurate RPM (+-10).
+    return vel * (600.0 / 2048.0);
+  }
+    public double getHeading() {
+      m_gyro.getYawPitchRoll(values);
+      return Math.IEEEremainder(values[0], 360) * (false ? -1.0 : 1.0);
+    }
+    public void resetOdometry(Pose2d pose) {
+      
+      m_odometry.resetPosition(pose, Rotation2d.fromDegrees(getHeading()));
+    }
     public void arcadeDrive(double fwd, double rot) {
       m_drive.arcadeDrive(fwd, rot);
     }
-    // public void resetEncoders() {
-    //   m_leftEncoder.reset();
-    //   m_rightEncoder.reset();
-    // }
     public void tankDriveVolts(double leftVolts, double rightVolts) {
       System.out.println("MOVING");
-      //right1.set(TalonFXControlMode., demand);
-      //left1.setVoltage(-rightVolts);
+      right1.set(TalonFXControlMode.Current,leftVolts);
+      left1.set(TalonFXControlMode.Current,-rightVolts);
     }
-    // public double getAverageEncoderDistance() {
-    //   return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
-    // }
+    public double getAverageEncoderDistance() {
+      return (getRPM(right1.getSensorCollection().getIntegratedSensorVelocity()) + getRPM(left1.getSensorCollection().getIntegratedSensorVelocity())) / 2.0;
+    }
     // public Encoder getLeftEncoder() {
     //   return m_leftEncoder;
     // }
