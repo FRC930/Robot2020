79b5ffc20f0486d8cd9611bffe109e9af46c7021
FrostBeast1 <frostbeast9@gmail.com>
2020-01-25 16:27:43 -0600

    Added logging to deadband math. The test files still don't work.

--- RobotCode2020New/src/main/java/frc/robot/utilities/DeadbandMath.java
+++ RobotCode2020New/src/main/java/frc/robot/utilities/DeadbandMath.java
@@ -1,5 +1,6 @@
 package frc.robot.utilities;
 
+import java.util.logging.*;
 /**
  * Shooter Subsystem
  */
@@ -21,7 +22,11 @@ public class DeadbandMath {
     private final double LINE_4_M = DISTANCE_TO_INNER / (COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
     private final double LINE_4_B = (DISTANCE_TO_INNER * -COMP_OUTER_RADIUS) / (COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
 
+    private final static Logger LOGGER =  
+                Logger.getLogger(Logger.DeadbandMath);
+    
     public DeadbandMath() {
+        logger.setLevel(Constants.LOG_LEVEL);
     }
 
     public static enum ShotType {
@@ -46,7 +51,11 @@ public class DeadbandMath {
      * @return {@link #shotType} set in {@link #calculateShotType}
      */
     public ShotType setPosition(double angle, double distanceFromGoal) {
-        return calculateShotType(Math.sin(Math.toRadians(angle)) * distanceFromGoal, Math.cos(Math.toRadians(angle)) * distanceFromGoal);
+        logger.entering(getClass().getName(), "<Method Name>");
+        ShotType shotType = calculateShotType(Math.sin(Math.toRadians(angle)) * distanceFromGoal, Math.cos(Math.toRadians(angle)) * distanceFromGoal);
+        logger.log(Level.INFO, "shotType = ", shotType);
+        logger.exiting(getClass().getName(), "<Method Name>");
+        return shotType;
     }
 
     /**
@@ -61,20 +70,28 @@ public class DeadbandMath {
      * @return {@link #shotType}
      */
     private ShotType calculateShotType(double distanceFromWall, double distanceFromCenter) {
+        logger.entering(getClass().getName(), "<Method Name>");
         double yOfLine2 = LINE_2_M * distanceFromCenter + LINE_2_B;
         double yOfLine3 = LINE_3_M * distanceFromCenter + LINE_3_B;
-        
         if (distanceFromWall >= yOfLine2 && distanceFromWall >= yOfLine3) {
+            logger.log(Level.INFO, "shotType = ", ShotType.SURE);
+            logger.exiting(getClass().getName(), "<Method Name>");
             return ShotType.SURE;
         } else { 
             double yOfLine1 = LINE_1_M * distanceFromCenter + LINE_1_B;
             if (distanceFromWall >= yOfLine1 && distanceFromWall < yOfLine2) {
+                logger.log(Level.INFO, "shotType = ", ShotType.MAYBE);
+                logger.exiting(getClass().getName(), "<Method Name>");
                 return ShotType.MAYBE;
             } else {
                 double yOfLine4 = LINE_4_M * distanceFromCenter + LINE_4_B;
                 if(distanceFromWall >= yOfLine4 && distanceFromWall < yOfLine3) {
+                    logger.log(Level.INFO, "shotType = ", ShotType.MAYBE);
+                    logger.exiting(getClass().getName(), "<Method Name>");
                     return ShotType.MAYBE;
                 } else {
+                    logger.log(Level.INFO, "shotType = ", ShotType.MISS);
+                    logger.exiting(getClass().getName(), "<Method Name>");
                     return ShotType.MISS;
                 }
             }
