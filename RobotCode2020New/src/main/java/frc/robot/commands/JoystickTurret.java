9d4099d7fcd27902af87c7dcd6f76a894de81fa8
Kyle Jambretz <kylejambretz@gmail.com>
2020-01-28 19:16:47 -0600

    changed sources

---
+++ RobotCode2020New/src/main/java/frc/robot/commands/JoystickTurret.java
@@ -0,0 +1,42 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.TurretSubsystem;
+
+/**
+ * TurretSpeedCommand is a temporary speed controller
+ */
+public class JoystickTurret extends CommandBase {
+    private TurretSubsystem turret;
+    private Joystick driver;
+
+    public JoystickTurret(TurretSubsystem turret, Joystick driver) {
+        this.turret = turret;
+        this.driver = driver;
+
+        addRequirements(turret);
+    }
+
+    @Override
+    public void execute() {
+        double speed = Math.pow(this.driver.getRawAxis(0), 3);
+        if (speed < 0) {
+            if (turret.getEncoderPosition() > 1500) {
+                speed = 0;
+            }
+        } else if (speed > 0) {
+            if (turret.getEncoderPosition() < -1500) {
+                speed = 0;
+            }
+        }
+        turret.setSpeed(speed);
+        SmartDashboard.putNumber("TurretRotation", turret.getEncoderPosition());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
