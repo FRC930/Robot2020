0b5f4285e2a62427c1553ef7e5ee166fabfc6fb3
Abbygottfried <gottabig@stu.masd.k12.wi.us>
2020-02-01 10:43:53 -0600

    owo

--- RobotCode2020New/src/main/java/frc/robot/commands/intakecommands/IntakeStopCommand.java
+++ RobotCode2020New/src/main/java/frc/robot/commands/intakecommands/IntakeStopCommand.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.commands.intakecommands;
 
 import frc.robot.Constants;
 
