09dac9cd7fee4d987c7ed81763880ef2f0c71d20
FrostBeast1 <frostbeast9@gmail.com>
2020-02-01 16:52:00 -0600

    Worked on shooter deadband and finshed tests so all ran.

--- RobotCode2020New/src/main/java/frc/robot/utilities/DeadbandMath.java
+++ RobotCode2020New/src/main/java/frc/robot/utilities/DeadbandMath.java
@@ -11,26 +11,27 @@ public class DeadbandMath {
     private final double OUTER_RING_RADIUS = 0.439928;
     private final double INNER_RING_RADIUS = 0.1651;
     private final double DISTANCE_TO_INNER = 0.74295;
+    private final double MAX_DISTANCE_FROM_GOAL = 12.9;
+    private final double MAX_DISTANCE_FROM_MIDLINE_LEFT = 5.805636;
+    private final double MAX_DISTANCE_FROM_MIDLINE_RIGHT = 2.404364;
     private final double COMP_OUTER_RADIUS = OUTER_RING_RADIUS - BALL_RADIUS;
     private final double COMP_INNER_RADIUS = INNER_RING_RADIUS - BALL_RADIUS;
     private final double LINE_1_M = DISTANCE_TO_INNER / (-COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
     private final double LINE_1_B = (DISTANCE_TO_INNER * COMP_OUTER_RADIUS) / (-COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
     private final double LINE_2_M = DISTANCE_TO_INNER / (COMP_INNER_RADIUS - COMP_OUTER_RADIUS);
     private final double LINE_2_B = (DISTANCE_TO_INNER * COMP_OUTER_RADIUS) / (COMP_INNER_RADIUS - COMP_OUTER_RADIUS);
-    private final double LINE_3_M = DISTANCE_TO_INNER / (COMP_OUTER_RADIUS + COMP_INNER_RADIUS);
-    private final double LINE_3_B = (DISTANCE_TO_INNER * -COMP_OUTER_RADIUS) * (COMP_OUTER_RADIUS + COMP_INNER_RADIUS);
-    private final double LINE_4_M = DISTANCE_TO_INNER / (COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
-    private final double LINE_4_B = (DISTANCE_TO_INNER * -COMP_OUTER_RADIUS) / (COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
-
-    private final static Logger logger =  
-                Logger.getLogger("George");
+    private final double LINE_3_M = DISTANCE_TO_INNER / (COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
+    private final double LINE_3_B = (DISTANCE_TO_INNER * -COMP_OUTER_RADIUS) / (COMP_OUTER_RADIUS - COMP_INNER_RADIUS);
+    private final double LINE_4_M = DISTANCE_TO_INNER / (COMP_OUTER_RADIUS + COMP_INNER_RADIUS);
+    private final double LINE_4_B = (DISTANCE_TO_INNER * -COMP_OUTER_RADIUS) / (COMP_OUTER_RADIUS + COMP_INNER_RADIUS);
+    private Logger logger = Logger.getLogger("George");
     
     public DeadbandMath() {
-        //logger.setLevel(Constants.LOG_LEVEL);
+        logger.setLevel(Level.FINER);
     }
 
     public static enum ShotType {
-        MISS(0), MAYBE(1), SURE(2);
+        ERROR(-1), MISS(0), MAYBE(1), BEST(2);
 
         private int type;
 
@@ -40,7 +41,7 @@ public class DeadbandMath {
     }
 
     /**
-     * Use this to set the position of the robot
+     * Use this to set the position of the robot and return the zone calculated.
      * <p>
      * The method {@link #calculateShotType} is called in this method also
      * 
@@ -51,10 +52,19 @@ public class DeadbandMath {
      * @return {@link #shotType} set in {@link #calculateShotType}
      */
     public ShotType setPosition(double angle, double distanceFromGoal) {
-        logger.entering(getClass().getName(), "<Method Name>");
-        ShotType shotType = calculateShotType(Math.sin(Math.toRadians(angle)) * distanceFromGoal, Math.cos(Math.toRadians(angle)) * distanceFromGoal);
-        logger.log(Level.INFO, "shotType = ", shotType);
-        logger.exiting(getClass().getName(), "<Method Name>");
+        logger.log(Level.INFO, "Log Level: " + logger.getLevel().toString()) ;
+        logger.entering(this.getClass().getName(), "setPosition");
+        ShotType shotType = null;
+        if (Math.abs(angle) >= 90 || distanceFromGoal <= 0) {
+            shotType = ShotType.ERROR;
+        } else {
+            final double robotX = Math.sin(Math.toRadians(angle)) * distanceFromGoal;
+            final double robotY = Math.cos(Math.toRadians(angle)) * distanceFromGoal;
+            System.out.println("Robot X:" + robotX + ": Robot Y:" + robotY);
+            shotType = calculateShotType(robotX, robotY);
+        }
+        logger.log(Level.INFO, "setPosition.shotType = " + shotType);
+        logger.exiting(this.getClass().getName(), "setPosition");
         return shotType;
     }
 
@@ -65,36 +75,53 @@ public class DeadbandMath {
      * This method will calculate the chance of making a shot in the inner goal.
      * 
      * @param distanceFromWall is the current distance from the wall.
-     * @param distanceFromCenter is the current distance from a line directly in front of the goal.
+     * @param distanceFromCenter is the current distance from a line perpendicular to the goal.
      * 
      * @return {@link #shotType}
      */
-    private ShotType calculateShotType(double distanceFromWall, double distanceFromCenter) {
-        logger.entering(getClass().getName(), "<Method Name>");
-        double yOfLine2 = LINE_2_M * distanceFromCenter + LINE_2_B;
-        double yOfLine3 = LINE_3_M * distanceFromCenter + LINE_3_B;
-        if (distanceFromWall >= yOfLine2 && distanceFromWall >= yOfLine3) {
-            logger.log(Level.INFO, "shotType = ", ShotType.SURE);
-            logger.exiting(getClass().getName(), "<Method Name>");
-            return ShotType.SURE;
-        } else { 
-            double yOfLine1 = LINE_1_M * distanceFromCenter + LINE_1_B;
-            if (distanceFromWall >= yOfLine1 && distanceFromWall < yOfLine2) {
-                logger.log(Level.INFO, "shotType = ", ShotType.MAYBE);
-                logger.exiting(getClass().getName(), "<Method Name>");
-                return ShotType.MAYBE;
-            } else {
-                double yOfLine4 = LINE_4_M * distanceFromCenter + LINE_4_B;
-                if(distanceFromWall >= yOfLine4 && distanceFromWall < yOfLine3) {
-                    logger.log(Level.INFO, "shotType = ", ShotType.MAYBE);
-                    logger.exiting(getClass().getName(), "<Method Name>");
-                    return ShotType.MAYBE;
+    private ShotType calculateShotType(double distanceFromCenter, double distanceFromWall) {
+        logger.entering(this.getClass().getName(), "calculateShotType");
+        ShotType shotType = null;
+
+        //
+        //  is this point on the field of play?
+        if (distanceFromWall > MAX_DISTANCE_FROM_GOAL || 
+            distanceFromWall <= 0 || 
+            distanceFromCenter >= MAX_DISTANCE_FROM_MIDLINE_RIGHT || 
+            distanceFromCenter <= -MAX_DISTANCE_FROM_MIDLINE_LEFT) {
+            shotType = ShotType.ERROR;
+        } else {
+            //
+            //  figure out if the point is in the "best" zone
+            double yOfLine2 = LINE_2_M * distanceFromCenter + LINE_2_B;
+            double yOfLine3 = LINE_3_M * distanceFromCenter + LINE_3_B;
+            System.out.println("Line 2 Y:" + yOfLine2 + ": Line 3 Y:" + yOfLine3);
+            if (distanceFromWall >= yOfLine2 && distanceFromWall >= yOfLine3) {
+                shotType = ShotType.BEST;
+            } else { 
+                //
+                //  figure out if we are in the left hand 'maybe' zone
+                double yOfLine1 = LINE_1_M * distanceFromCenter + LINE_1_B;
+                System.out.println("Line 1 Y:" + yOfLine1);
+                if (distanceFromWall >= yOfLine1 && distanceFromWall < yOfLine2) {
+                    shotType = ShotType.MAYBE;
                 } else {
-                    logger.log(Level.INFO, "shotType = ", ShotType.MISS);
-                    logger.exiting(getClass().getName(), "<Method Name>");
-                    return ShotType.MISS;
+                    //
+                    // figure out if we are in the right hand 'maybe' zone
+                    double yOfLine4 = LINE_4_M * distanceFromCenter + LINE_4_B;
+                    System.out.println("Line 4 Y:" + yOfLine4);
+                    if(distanceFromWall >= yOfLine4 && distanceFromWall < yOfLine3) {
+                        shotType = ShotType.MAYBE;
+                    } else {
+                        //
+                        // since it is not in any other zone, it is a miss
+                        shotType = ShotType.MISS;
+                    }
                 }
             }
         }
+        logger.log(Level.INFO, "calculateShotType.shotType = " + shotType);
+        logger.exiting(this.getClass().getName(), "calculateShotType");
+        return shotType;
     }
 }
