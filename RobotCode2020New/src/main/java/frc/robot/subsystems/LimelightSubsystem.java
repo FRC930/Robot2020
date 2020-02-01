f0b2bb0af4d47e56abf966727c1ad552197fed83
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2020-02-01 08:28:06 -0600

    Removed unused imports Made project version newer Removed wonky merge lines in .project

--- RobotCode2020New/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -7,16 +7,14 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import main.java.frc.robot.utilities.FIFOStack;
-
-import java.util.Deque;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import frc.robot.utilities.FIFOStack;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
 
--- RobotCode2020New/src/main/java/frc/robot/utilities/FIFOStack.java
+++ RobotCode2020New/src/main/java/frc/robot/utilities/FIFOStack.java
@@ -1,4 +1,4 @@
-package main.java.frc.robot.utilities;
+package frc.robot.utilities;
 
 import java.util.ArrayList;
 import java.util.List;
