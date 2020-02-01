94cb5f5cb785007500e086b7b032cf21bbf0ae34
Noah <noahafairchild@gmail.com>
2020-02-01 10:35:23 -0600

    Thats good

---
+++ RobotCode2020New/src/main/java/frc/robot/commands/autocommands/AutonomousCommand.java
@@ -0,0 +1,103 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.autocommand;
+
+
+import edu.wpi.first.wpilibj.controller.RamseteController;
+
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.controller.PIDController;
+
+import edu.wpi.first.wpilibj2.command.*;
+
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.Constants;
+
+import java.util.List;
+public class AutonomousCommand extends SequentialCommandGroup {
+  /**
+   * Creates a new Autonomous.
+   */
+  DriveSubsystem m_drive;
+  public AutonomousCommand(DriveSubsystem subsystem) {
+    m_drive = subsystem;
+    var autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(Constants.KSVOLTS,
+            Constants.KVVOLT,
+            Constants.KAVOLT),
+            Constants.KDRIVEKINEMATICS,10);
+    
+    TrajectoryConfig config =
+      new TrajectoryConfig(Constants.KMAXSPEED,
+      Constants.KMAXACCELERATION)
+      // Add kinematics to ensure max speed is actually obeyed
+      .setKinematics(Constants.KDRIVEKINEMATICS)
+      // Apply the voltage constraint
+      .addConstraint(autoVoltageConstraint);
+
+  Trajectory trajectory1 = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(),
+        //new Translation2d(1, 2)),
+        // End 1 meters straight ahead of where we started, facing forward
+        new Pose2d(1, -1, new Rotation2d(270)),
+        // Pass config
+        config);
+    RamseteCommand ramseteCommand1 = new RamseteCommand(
+        trajectory1,
+        m_drive::getPose,
+        new RamseteController(Constants.KRAMSETEB, Constants.KRAMSETEZETA),
+        new SimpleMotorFeedforward(Constants.KSVOLTS,
+                                   Constants.KVVOLT,
+                                   Constants.KAVOLT),
+        Constants.KDRIVEKINEMATICS,
+        m_drive::getWheelSpeeds,
+        new PIDController(Constants.KPDRIVEVEL, 0, 0),
+        new PIDController(Constants.KPDRIVEVEL, 0, 0),
+        // RamseteCommand passes volts to the callback
+        m_drive::tankDriveVolts,
+        m_drive
+    );
+    Trajectory trajectory2 = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(),
+        //new Translation2d(1, 2)),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(2, 1, new Rotation2d(270)),
+        // Pass config
+        config);
+     RamseteCommand ramseteCommand2 = new RamseteCommand(
+        trajectory2,
+        m_drive::getPose,
+        new RamseteController(Constants.KRAMSETEB, Constants.KRAMSETEZETA),
+        new SimpleMotorFeedforward(Constants.KSVOLTS,
+                                   Constants.KVVOLT,
+                                   Constants.KAVOLT),
+        Constants.KDRIVEKINEMATICS,
+        m_drive::getWheelSpeeds,
+        new PIDController(Constants.KPDRIVEVEL, 0, 0),
+        new PIDController(Constants.KPDRIVEVEL, 0, 0),
+        // RamseteCommand passes volts to the callback
+        m_drive::tankDriveVolts,
+        m_drive
+    );
+    addCommands(ramseteCommand1,new WaitCommand(5), ramseteCommand2);
+  }
+}
---
+++ RobotCode2020New/src/main/java/frc/robot/commands/drivecommands/DriveCommand.java
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.drivecommands;
+
+import frc.robot.Constants;
+
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DriveCommand extends CommandBase {
+
+  //-------- CONSTANTS --------\\
+
+  //-------- DECLARATIONS --------\\
+  private final DriveSubsystem driveSubsystem;
+  private final Joystick m_driveStick;
+
+  //-------- CONSTRUCTOR --------\\
+
+  public DriveCommand(DriveSubsystem dSubsystem, Joystick driverStick) {
+    driveSubsystem = dSubsystem;
+    m_driveStick = driverStick;
+    addRequirements(dSubsystem);  // Use addRequirements() here to declare subsystem dependencies.
+  } 
+  //-------- COMMANDBASE METHODS --------\\
+  @Override   // Called when the command is initially scheduled.
+  public void initialize() {
+  }
+
+  @Override   // Called every time the scheduler runs while the command is scheduled.
+  public void execute() {  
+    run(m_driveStick.getRawAxis(Constants.DRIVE_AXIS_RIGHT_X), m_driveStick.getRawAxis(Constants.DRIVE_AXIS_LEFT_Y));
+  }
+  @Override   // Called once the command ends or is interrupted.
+  public void end(boolean interrupted) {
+  }
+
+
+  /*
+  //We comment out isFinished so we always have control of the drive and it gives contorl up easier
+  @Override   // Returns true when the command should end.
+  public boolean isFinished() {
+    return false;
+  }
+
+  */
+
+  //-------- METHODS --------\\
+  private void run(double stickX, double stickY) {
+        
+    // Cubing values to create smoother function
+
+    stickX = -Math.pow(stickX, 3);
+    stickY = Math.pow(stickY, 3);
+
+    // Multiplies for smoothing turning
+    stickX *= Constants.DRIVE_TURNING_MULTIPLIER;
+
+    // Dead band after the cube sets to zero if under the dead band
+    if (Math.abs(stickX) < Constants.DRIVE_DEADBAND_JOYSTICK) {
+      stickX = 0;
+    }
+    if (Math.abs(stickY) < Constants.DRIVE_DEADBAND_JOYSTICK) {
+      stickY = 0;
+    }
+    // Arcade drive
+    driveSubsystem.runAt((stickY + stickX), -(stickY - stickX));
+  } //End of method run()
+} //End of class DriveCommand
