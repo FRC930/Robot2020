2458a6508ee3c91ac2fe7179401aa8b5668a1fed
mennmark <xnewbly@gmail.com>
2020-02-03 20:02:32 -0600

    Logging and comments for HopperSubsystem

--- RobotCode2020New/src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -5,9 +5,9 @@ import java.util.logging.Logger;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
  
 public class HopperSubsystem extends SubsystemBase {
@@ -16,21 +16,36 @@ public class HopperSubsystem extends SubsystemBase {
     
     public double speed;
 
+    private Logger logger;
+
     /**
     * The default constructor
     */
     public HopperSubsystem() {
         HopperMotor = new VictorSPX(Constants.HOPPER_MOTOR_PORT);
+
+        logger = Logger.getLogger(HopperSubsystem.class.getName());
     }
 
     //sets the speed to the Hoppermotor
     public void setSpeed(double speed){
+        //Logs the setSpeed method as INFO returning, "setSpeed()"
+        logger.entering(getClass().getName(), "setSpeed()");
+
         this.speed = speed;
         HopperMotor.set(ControlMode.PercentOutput, speed);
+
+        logger.log(Level.INFO, "Set shooter speed to " + speed);
+        logger.exiting(getClass().getName(), "setSpeed()");
     }
 
     // returns the speed
     public double getSpeed(){
+        //Logs the getSpeed method as INFO returning, "getSpeed()"
+        logger.entering(getClass().getName(), "getSpeed()");
+        logger.log(Level.INFO, "Get shooter speed to " + speed);
+        logger.exiting(getClass().getName(), "getSpeed()");
+
         return this.speed;
     }
 }
