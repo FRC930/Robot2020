73642a59c5281659a345fbe64b94a761ded36b68
Kyle Sene <soupcateatternom@gmail.com>
2020-02-05 17:59:28 -0600

    Removed an unused import

--- RobotCode2020New/src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
