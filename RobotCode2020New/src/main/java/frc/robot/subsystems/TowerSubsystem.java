1a161c5aec6a7e8ad834a7ec0c2fea8a74642f3e
Kyle Sene <soupcateatternom@gmail.com>
2020-02-05 19:23:18 -0600

    Removed duplicate commands from Jabroni's subteam

--- RobotCode2020New/src/main/java/frc/robot/subsystems/TowerSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/TowerSubsystem.java
@@ -8,36 +8,52 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TowerSubsystem extends SubsystemBase {
+  
+    //-------- CONSTANTS --------\\
+
+    //-------- DECLARATIONS --------\\
+
     // VictorSPX is a motor controller that makes the conveor belt  
     //Take's the power cell up to the shooter
     public VictorSPX TowerMotor;
+
     private Logger logger = Logger.getLogger(this.getClass().getName());
-    private double speed;
+
+    private double speed;   //TODO: Remove this and use the getSpeed method from VictorSPX class
+
+    //-------- CONSTRUCTOR --------\\
 
     public TowerSubsystem() {
-        TowerMotor = new VictorSPX(4);
+        TowerMotor = new VictorSPX(4);  //TODO: Turn the 4 into a constant
         logger.setLevel(Level.FINE);
-
-        //sets the speed to the tower motor 
+    }   
     
+    //-------- METHODS --------\\
+
+    //sets the speed to the tower motor 
     public void setSpeed(double speed){
         logger.entering(this.getClass().getName(), "setSpeed");
         logger.log(Level.INFO, "motorSpeed: "+ speed );
+
         this.speed = speed;
+
         TowerMotor.set(ControlMode.PercentOutput, speed);
         logger.exiting (this.getClass().getName(), "setSpeed");
     }
+
     // returns the speed from the tower motor
-    public double getSpeed(){
-    
+    public double getSpeed(){ 
         logger.entering(this.getClass().getName(),"getSpeed");
         logger.log(Level.INFO, "motorSpeed: " + speed);
         logger.exiting (this.getClass().getName(),"getSpeed");
+        
         return this.speed;
     }
 
+    //stops the motor
     public void stopMotor() {
         this.speed = 0;
         TowerMotor.set(ControlMode.PercentOutput, 0);
     }
+
 } //end of class 
