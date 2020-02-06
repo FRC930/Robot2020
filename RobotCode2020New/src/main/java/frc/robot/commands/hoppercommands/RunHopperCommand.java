fe85e394babe12a3da0e00431d5c2e6c34954213
Andrew Ocampo <aocampo2862@gmail.com>
2020-02-05 18:54:16 -0600

    hopper and tower commands

--- RobotCode2020New/src/main/java/frc/robot/commands/hoppercommands/RunHopperCommand.java
+++ RobotCode2020New/src/main/java/frc/robot/commands/hoppercommands/RunHopperCommand.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-package frc.robot.commands.shootercommands;
+package frc.robot.commands.hoppercommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.HopperSubsystem;
