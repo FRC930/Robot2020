cec976c50a716f4c2f18084e37b7d93b52e16903
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2020-02-06 19:43:51 -0600

    LEDs

--- RobotCode2020New/src/main/java/frc/robot/commands/AllianceLEDCommand.java
+++ RobotCode2020New/src/main/java/frc/robot/commands/AllianceLEDCommand.java
@@ -10,6 +10,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.LEDSubsystem;
 
@@ -22,8 +23,7 @@ public class AllianceLEDCommand extends CommandBase {
     private int counter;
     private int ledPosition;
 
-    public AllianceLEDCommand(LEDSubsystem ledSubsystem)
-    {
+    public AllianceLEDCommand(LEDSubsystem ledSubsystem) {
         m_ledSubsystem = ledSubsystem;
         addRequirements(m_ledSubsystem);
 
@@ -32,102 +32,45 @@ public class AllianceLEDCommand extends CommandBase {
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() 
-    {
+    public void initialize() {
         buffer = new AddressableLEDBuffer(60);
         ledPosition = 0;
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() 
-    {
-        if(counter >= 25)
-        {
-            if(DriverStation.getInstance().getAlliance() == Alliance.Red)
-            {
-                if(ledPosition == 0)
-                {
-                    for(int i = 0; i < buffer.getLength(); i++)
-                    {
-                        if((i % 2) == 0)
-                        {
-                            buffer.setRGB(i, 255, 0, 0);
-                            ledPosition = 1;
-                        }
+    public void execute() {
+        SmartDashboard.putNumber("Counter", counter);
+        if (counter >= 25) {
+            m_ledSubsystem.resetLEDs();
+            if (DriverStation.getInstance().getAlliance() == Alliance.Red) {
+                if (ledPosition == 0) {
+                    for (int i = 0; i < buffer.getLength(); i += 2) {
+                        buffer.setRGB(i, 255, 0, 0);
+                        ledPosition = 1;
                     }
-                    //m_ledSubsystem.resetLEDs();
-                }
-                else if(ledPosition == 1)
-                {
-                    for(int i = 1; i < buffer.getLength(); i++)
-                    {
-                        if((i % 2) == 0)
-                        { 
-                            buffer.setRGB(i, 225, 140, 0);
-                            ledPosition = 2;
-                        }
+                } else if (ledPosition == 1) {
+                    for (int i = 1; i < buffer.getLength(); i += 2) {
+                        buffer.setRGB(i, 225, 140, 0);
+                        ledPosition = 2;
                     }
-                    //m_ledSubsystem.resetLEDs();
                 }
-                else
-                {
-                    for(int i = 1; i < buffer.getLength(); i++)
-                    {
-                        if((i % 2) == 0)
-                        { 
-                            buffer.setRGB(i, 255, 255, 0);
-                            ledPosition = 0;
-                        }
+            } else if (DriverStation.getInstance().getAlliance() == Alliance.Blue) {
+                if (ledPosition == 0) {
+                    for (int i = 0; i < buffer.getLength(); i += 2) {
+                        buffer.setRGB(i, 0, 0, 255);
                     }
-                    //m_ledSubsystem.resetLEDs();
+                    ledPosition = 1;
+                } else if (ledPosition == 1) {
+                    for (int i = 1; i < buffer.getLength(); i += 2) {
+                        buffer.setRGB(i, 0, 140, 225);
+                    }
+                    ledPosition = 0;
                 }
-
-                m_ledSubsystem.updateBuffer(buffer);
-                counter = 0;
             }
-            else if(DriverStation.getInstance().getAlliance() == Alliance.Blue)
-            {
-                if(ledPosition == 0)
-                {
-                    for(int i = 0; i < buffer.getLength(); i++)
-                    {
-                        if((i % 2) == 0)
-                        {
-                            buffer.setRGB(i, 0, 0, 255);
-                            ledPosition = 1;
-                        }
-                    }
-                    //m_ledSubsystem.resetLEDs();
-                }
-                else if(ledPosition == 1)
-                {
-                    for(int i = 1; i < buffer.getLength(); i++)
-                    {
-                        if((i % 2) == 0)
-                        { 
-                            buffer.setRGB(i, 0, 140, 225);
-                            ledPosition = 2;
-                        }
-                    }
-                    //m_ledSubsystem.resetLEDs();
-                }
-                else
-                {
-                    for(int i = 1; i < buffer.getLength(); i++)
-                    {
-                        if((i % 2) == 0)
-                        { 
-                            buffer.setRGB(i, 0, 255, 255);
-                            ledPosition = 0;
-                        }
-                    }
-                    //m_ledSubsystem.resetLEDs();
-                }
-
-                m_ledSubsystem.updateBuffer(buffer);
-                counter = 0;
-            }
+            
+            m_ledSubsystem.updateBuffer(buffer);
+            counter = 0;
         }
 
         counter++;
@@ -135,8 +78,7 @@ public class AllianceLEDCommand extends CommandBase {
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) 
-    {
+    public void end(boolean interrupted) {
 
     }
 
