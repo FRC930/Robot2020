e2adc3c8a8f7812f32d22f791fd1c9fde1f0bef5
FrostBeast1 <frostbeast9@gmail.com>
2020-02-08 18:38:30 -0600

    Forgot curly bracket.

--- RobotCode2020New/src/main/java/frc/robot/utilities/DeadbandMath.java
+++ RobotCode2020New/src/main/java/frc/robot/utilities/DeadbandMath.java
@@ -102,6 +102,7 @@ public class DeadbandMath {
                     shotType = ShotType.MISS;
                 }
             }
+        }
         logger.log(Level.INFO, "Final ShotType: " + shotType);
         logger.exiting(this.getClass().getName(), "calculateShotType");
         return shotType;
