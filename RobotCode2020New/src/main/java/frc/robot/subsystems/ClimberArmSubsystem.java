8cb10b9ff56260c46042f5efc5083fd52d423efb
Alexfrey3570 <codifrey51@gmail.com>
2020-02-25 18:14:18 -0600

    i have no idea what i did to make a change but i guess i made this thing, i think?

--- RobotCode2020New/src/main/java/frc/robot/subsystems/ClimberArmSubsystem.java
+++ RobotCode2020New/src/main/java/frc/robot/subsystems/ClimberArmSubsystem.java
@@ -17,6 +17,10 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//--------COMMANDS--------\\
+
+
+
 //--------SUBSYSTEM CLASS--------\\
 
 public class ClimberArmSubsystem extends SubsystemBase {
