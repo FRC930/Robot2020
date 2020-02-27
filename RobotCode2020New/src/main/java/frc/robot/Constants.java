e5289360bfce4481c82e1cc0e670bf122cd0c3d4
Andrew Ocampo <aocampo2862@gmail.com>
2020-02-26 18:06:46 -0600

    added toggle shift command, still need to bind it

--- RobotCode2020New/src/main/java/frc/robot/Constants.java
+++ RobotCode2020New/src/main/java/frc/robot/Constants.java
@@ -51,6 +51,7 @@ public final class Constants {
 
     public static final int INTAKE_SOLENOID_ID = 0;
     public static final int SHOOTER_SOLENOID_ID = 1;
+    public static final int SHIFTER_SOLENOID_ID = 2;
 
     public static final int COMPRESSOR_PORT = 0;
 
